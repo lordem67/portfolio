808bf1b227ec2679cadd45f383f08b1a28bb2ad0
lordem67 <134222175+lordem67@users.noreply.github.com>
2025-04-28 20:25:30 +0100

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(), react()],
-  base: './',
+  base: process.env.VITE_BASE_PATH || "/portfolio",
 })
 
 
